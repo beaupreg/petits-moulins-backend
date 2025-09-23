a130893f5ff694be99bfe3cebc9761a59efc5659
beaupreg <beaupreg@gmail.com>
2025-09-23 09:58:50 -0400

    Updated pool pg in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const { Pool } = require('pg');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
